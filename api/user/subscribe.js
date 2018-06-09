60a9f3a0e2da3b4d4ab7834dd82c0c6426685df5
guangboshushu <h28846019@126.coml>
2018-06-10 07:14:54 +0800

    add

--- api/user/subscribe.js
+++ api/user/subscribe.js
@@ -1,7 +1,13 @@
 const insertMysql = require('../../mysql/insertMysql.js')
 
-const subscribe = (data, callback) => {
-  console.log('111')
+const subscribe = (req, callback) => {
+  console.log('data', req.body)
+  let subscribe = req.query.subscribe
+  let data = req.body
+  console.log('data', req.body)
+  console.log('data', subscribe)
+  data.regtime = new Date().getTime()
+  if (!subscribe)
   insertMysql('subscribe', data, res => {
     console.log('222')
     callback(res)
