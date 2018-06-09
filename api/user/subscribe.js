84c4092b4cc5e26be7dfbb6c529327108722c4a0
guangboshushu <h28846019@126.coml>
2018-06-10 07:22:28 +0800

    add

--- api/user/subscribe.js
+++ api/user/subscribe.js
@@ -4,7 +4,8 @@ const subscribe = (req, callback) => {
   let subscribe = req.query.subscribe
   let data = req.body
   data.regtime = new Date().getTime()
-  if (!subscribe) {
+  if (subscribe === 0) {
+    console.log('subscribe=0')
     insertMysql('subscribe', data, res => {
       console.log('222')
       callback(res)
--- router/myRouterPost.js
+++ router/myRouterPost.js
@@ -1,7 +1,6 @@
 const subscribe = require('../api/user/subscribe')
 
 const routeSubscribe = (req, res, callback) => {
-  console.log('routeSubscribe')
   subscribe(req, (e) => {
     console.log('subscribe')
     callback(e)
