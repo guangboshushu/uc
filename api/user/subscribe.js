caaf8dd100de7939475adf50191e044be34d4df6
guangboshushu <h28846019@126.coml>
2018-06-10 07:29:59 +0800

    add

--- api/user/subscribe.js
+++ api/user/subscribe.js
@@ -4,6 +4,7 @@ const subscribe = (req, callback) => {
   let subscribe = req.query.subscribe
   let data = req.body
   data.regtime = new Date().getTime()
+  console.log('action', subscribe)
   if (subscribe === 0) {
     console.log('subscribe=0')
     insertMysql('subscribe', data, res => {
